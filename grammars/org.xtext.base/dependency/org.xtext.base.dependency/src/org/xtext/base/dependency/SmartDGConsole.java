13861b5900c55858ab2434a03cf058e68cbeccbc
VNagrath <nagrath@hs-ulm.de>
2020-10-29 18:39:46 +0100

    SMartDGConsole class added

--- grammars/org.xtext.base/dependency/org.xtext.base.dependency/src/org/xtext/base/dependency/SmartDGConsole.java
+++ grammars/org.xtext.base/dependency/org.xtext.base.dependency/src/org/xtext/base/dependency/SmartDGConsole.java
@@ -11,33 +11,32 @@ public class SmartDGConsole {
 	String name;
 	private MessageConsole console = null;
 	private MessageConsoleStream out = null;
-	
-	// https://wiki.eclipse.org/FAQ_How_do_I_write_to_the_console_from_a_plug-in%3F  START
-	private MessageConsole findConsole() {
-		ConsolePlugin plugin = ConsolePlugin.getDefault();
-		IConsoleManager conMan = plugin.getConsoleManager();
-		IConsole[] existing = conMan.getConsoles();
-		for (int i = 0; i < existing.length; i++)
-			if (name.equals(existing[i].getName()))
-				return (MessageConsole) existing[i];
-		// no console found, so create a new one
+
+	private MessageConsole init() {
+		ConsolePlugin consolePlugin = ConsolePlugin.getDefault();
+		IConsoleManager consoleManager = consolePlugin.getConsoleManager();
+		IConsole[] all_consoles = consoleManager.getConsoles();
+		for (int i = 0; i < all_consoles.length; i++) {
+			if (name.equals(all_consoles[i].getName())) {
+				return (MessageConsole) all_consoles[i];
+			}
+		}
 		MessageConsole myConsole = new MessageConsole(name, null);
-		conMan.addConsoles(new IConsole[] { myConsole });
+		consoleManager.addConsoles(new IConsole[] { myConsole });
 		return myConsole;
 	}
-	// https://wiki.eclipse.org/FAQ_How_do_I_write_to_the_console_from_a_plug-in%3F  END
-	
-	public SmartDGConsole(String in_name)	{
+
+	public SmartDGConsole(String in_name) {
 		name = in_name;
-		console = findConsole();
+		console = init();
 		out = console.newMessageStream();
 	}
-	
-	public void println(String text)	{
+
+	public void println(String text) {
 		out.println(text);
 	}
-	
-	public void print(String text)	{
+
+	public void print(String text) {
 		out.print(text);
-	}	
+	}
 }
