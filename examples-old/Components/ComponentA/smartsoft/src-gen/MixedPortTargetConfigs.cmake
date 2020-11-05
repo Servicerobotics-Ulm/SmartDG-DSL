059b0bd757374244d7e01a18f9ac0ed41e2f322e
VNagrath <nagrath@hs-ulm.de>
2020-11-05 17:46:59 +0100

    Removed old examples

--- examples-old/Components/ComponentA/smartsoft/src-gen/MixedPortTargetConfigs.cmake
+++
@@ -1,6 +0,0 @@
-# target configurations for PlainOpcUaComponentAExtension
-IF(Open62541CppWrapper_FOUND)
-TARGET_LINK_LIBRARIES(${PROJECT_NAME} Open62541CppWrapper)
-TARGET_COMPILE_DEFINITIONS(${PROJECT_NAME} PUBLIC HAS_OPCUA)
-ENDIF(Open62541CppWrapper_FOUND)
-
